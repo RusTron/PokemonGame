58ece1e5544e319366009cb90db1a5ac20e942ef
Ruslan Tron <rusan.tron@gmail.com>
2021-05-13 00:47:10 +0300

    Eslint & Prettier was added

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,56 @@
-const { resolve } = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { resolve } = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-const NODE_ENV = process.env.NODE_ENV;
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  mode: NODE_ENV ? NODE_ENV : "development",
-  entry: resolve(__dirname, "src/index.js"),
+  mode: isDev ? 'development' : 'production',
+  entry: resolve(__dirname, 'src/index.tsx'),
   output: {
-    path: resolve(__dirname, "dist"),
-    filename: "main.js",
+    path: resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
   },
   devServer: {
     port: 3000,
     open: true,
     hot: true,
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   plugins: [
     new HTMLWebpackPlugin({
-      template: resolve(__dirname, "public/index.html"),
+      template: resolve(__dirname, 'public/index.html'),
     }),
   ],
   module: {
     rules: [
       {
         test: /\.[tj]sx?$/,
-        use: ["ts-loader"],
         exclude: /node_modules/,
+        use: ['ts-loader'],
       },
       {
         test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
         use: [
-          "style-loader",
+          'style-loader',
+          'css-modules-typescript-loader?modules',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               modules: {
-                mode: "local",
-                localIdentName: "[path]__[name]__[local]__[hash:base64:5]",
-                auto: /\.modules\.\w+$/i
+                mode: 'local',
+                localIdentName: '[path]__[name]__[local]__[hash:base64:5]',
+                auto: /\.module\.\w+$/i,
               },
             },
           },
+          'sass-loader',
         ],
       },
     ],
